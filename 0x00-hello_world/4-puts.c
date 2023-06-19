052ef6cff53784e5863a43352d831caf34494fa7
Silas003 <silaskumi4@gmail.com>
2023-06-18 18:05:21 -0700

    low level

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return : Always 0 (success)
+ * /
+ int main(void)
+ {
+ 	puts("\"Programming is like a building a multingual puzzle");
+	return (0);
+}
